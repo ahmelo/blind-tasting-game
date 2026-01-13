647ae98a9587a804d22c8aaabc029d65f57ee7f5
Andre Melo <andrehenriquemelo@gmail.com>
2026-01-13 20:28:15 -0300

    Correção de importação e adição de CORS para Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:4173",  
         "http://127.0.0.1:4173",
-        "https://blind-tasting-game.vercel.app/",  # vamos ajustar depois
+        "https://blind-tasting-game.vercel.app",  # vamos ajustar depois
 
     ],
     allow_credentials=True,
